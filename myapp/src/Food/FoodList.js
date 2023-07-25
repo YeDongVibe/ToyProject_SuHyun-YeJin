0515c1243edbbf74719f716b7703ac082a908c06
YeDongVibe <wkwhstladmss@naver.com>
2023-07-25 17:52:14 +0900

    23.07.25

--- myapp/src/Food/FoodList.js
+++ myapp/src/Food/FoodList.js
@@ -2,24 +2,6 @@ const FoodList = () => {
 
     return(
         <main>
-            <div>
-                <input type="text" placeholder="키워드를 입력하세요" />
-                <button type="submit">Search</button>
-            </div>
-            <div>
-            <table>
-                    <thead>
-                        <tr>
-                            <th scope="col">자료구분 코드</th>
-                            <th scope="col">예측일자</th>
-                            <th scope="col">예측시간</th>
-                            <th scope="col">예보 값</th>
-                        </tr>                        
-                    </thead>
-                </table>
-            </div>
-            
-
         </main>
     )
 }
