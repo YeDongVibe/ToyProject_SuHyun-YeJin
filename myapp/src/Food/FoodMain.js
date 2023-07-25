459bfa8c3987f395305df4e2a5348927413da302
YeDongVibe <wkwhstladmss@naver.com>
2023-07-25 17:27:03 +0900

    23.07.25 Front첫화면

--- myapp/src/Food/FoodMain.js
+++ myapp/src/Food/FoodMain.js
@@ -1,10 +1,15 @@
-
+import style from "./Food.module.css"
+import MainCenterImg from "../Img/FoodMain/MainCenterImg.png"
+import MainTextImg from "../Img/FoodMain/MainTextImg"
 const FoodMain = () => {
 
-    return(
+    return (
         <main>
-            <div className = "MainImg">
-                <img src = "./"></img>
+            <div className={style.MainImg}>
+                <div className={style.CenterCon}>
+                    <img src={MainCenterImg} alt="MainCenterImg" className={style.MainCenterImg} />
+                    <img src={MainTextImg} alt="MainTextImg" className={style.Maintext} />
+                </div>
             </div>
         </main>
     )
