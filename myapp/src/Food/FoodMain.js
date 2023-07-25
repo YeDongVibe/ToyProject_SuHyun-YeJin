661ca4ad91015224680efa231d72a334bfd96b26
YeDongVibe <wkwhstladmss@naver.com>
2023-07-25 17:50:16 +0900

    23.07.25

--- myapp/src/Food/FoodMain.js
+++ myapp/src/Food/FoodMain.js
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom"
+
 import style from "./Food.module.css"
 import MainCenterImg from "../Img/FoodMain/MainCenterImg.png"
-import MainTextImg from "../Img/FoodMain/MainTextImg"
+import MainTextImg from "../Img/FoodMain/MainTextImg.png"
+import MainButtonImg from "../Img/FoodMain/MainButtonImg.png"
 const FoodMain = () => {
 
     return (
@@ -10,7 +13,13 @@ const FoodMain = () => {
                     <img src={MainCenterImg} alt="MainCenterImg" className={style.MainCenterImg} />
                     <img src={MainTextImg} alt="MainTextImg" className={style.Maintext} />
                 </div>
+                <Link to="/FoodList">
+                    <div>
+                        <img src={MainButtonImg} alt="MainButtonImg" className={style.MainButtonImg} />
+                    </div>
+                </Link>
             </div>
+
         </main>
     )
 }
