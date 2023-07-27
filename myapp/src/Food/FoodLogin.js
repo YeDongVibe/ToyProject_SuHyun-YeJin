81c1984f8438cbb75fccd5226bc497af63d2821a
YeDongVibe <wkwhstladmss@naver.com>
2023-07-27 17:50:17 +0900

    23.07.27

--- myapp/src/Food/FoodLogin.js
+++ myapp/src/Food/FoodLogin.js
@@ -1,6 +1,3 @@
-import React, { useState } from "react";
-import axios from "axios";
-
 import style from "./Food.module.css"
 
 import LoginBt from "../Img/FoodLogin/LoginBt.png"
@@ -8,48 +5,23 @@ import LoginID from "../Img/FoodLogin/LoginID.png"
 import LoginPW from "../Img/FoodLogin/LoginPW.png"
 
 const FoodLogin = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("/api/login", {
-                username,
-                password,
-            });
-            // 로그인 성공 시 처리
-        } catch (error) {
-            // 로그인 실패 시 처리
-        }
-    };
 
     return (
-        <div className={style.LoginBackImg}>
-            <div className={style.inputcontainer}>
-                <img src={LoginID} alt="LoginID" className="LoginID" />
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Username"
-                    className="login-input"
-                />
-            </div>
-            <div className={style.inputcontainer}>
-                <img src={LoginPW} alt="LoginPW" className="LoginPW" />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                    className="login-input"
-                />
-            </div>
-            <button type="submit" onClick={handleLogin} className={style.loginButton}>
-                <img src={LoginBt} alt="LoginBt" className="LoginBt" />
-            </button>
-        </div>
+        <main className={style.LoginBackImg}>
+            <form>
+                <div class="grid">
+                    <label for="ID" >
+                        <input type="text" id="ID" name="ID" placeholder="ID" required />
+                    </label>
+
+                    <label for="pw">
+                        <input type="text" id="pw" name="pw" placeholder="pw" required />
+                    </label>
+                </div>
+                <button  type="submit"><img src={LoginBt} alt="LoginBt" className={style.LoginBt}/></button>
+            </form>
+        </main>
+
     );
 };
 
