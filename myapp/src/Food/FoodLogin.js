5fbbb627a0bf6d2b73cb7929a1972eae1f8d7366
YeDongVibe <wkwhstladmss@naver.com>
2023-07-28 16:41:14 +0900

    23.07.28

--- myapp/src/Food/FoodLogin.js
+++ myapp/src/Food/FoodLogin.js
@@ -1,28 +1,52 @@
-import style from "./Food.module.css"
-
-import LoginBt from "../Img/FoodLogin/LoginBt.png"
-import LoginID from "../Img/FoodLogin/LoginID.png"
-import LoginPW from "../Img/FoodLogin/LoginPW.png"
+import style from "./Food.module.css";
+import LoginBt from "../Img/FoodLogin/LoginBt.png";
+import LoginID from "../Img/FoodLogin/LoginID.png";
+import LoginPW from "../Img/FoodLogin/LoginPW.png";
+import { useState } from "react"; // Import useState to handle login validation state
+import { useNavigate } from "react-router-dom";
 
 const FoodLogin = () => {
+  const navigate = useNavigate();
 
-    return (
-        <main className={style.LoginBackImg}>
-            <form>
-                <div class="grid">
-                    <label for="ID" >
-                        <input type="text" id="ID" name="ID" placeholder="ID" required />
-                    </label>
+  // State to store login status (true for successful login, false for failed login)
+  const [loginStatus, setLoginStatus] = useState(null);
 
-                    <label for="pw">
-                        <input type="text" id="pw" name="pw" placeholder="pw" required />
-                    </label>
-                </div>
-                <button  type="submit"><img src={LoginBt} alt="LoginBt" className={style.LoginBt}/></button>
-            </form>
-        </main>
+  const handleLogin = () => {
+    // 여기서 로그인 동작을 수행합니다.
+    // 예시로 로그인이 성공했다고 가정하고 setTimeout을 사용하여 로그인 결과를 1초 뒤에 설정합니다.
+    // 실제로는 서버와 통신하여 로그인 결과를 처리해야 합니다.
+    setTimeout(() => {
+      const loginResult = true; // 예시: 로그인 결과 (true: 성공, false: 실패)
 
-    );
+      // 로그인 결과에 따라 처리
+      if (loginResult) {
+        setLoginStatus(true); // 로그인 성공
+        navigate("/"); // FoodMain.js 경로로 페이지 이동
+      } else {
+        setLoginStatus(false); // 로그인 실패
+      }
+    }, 1000); // 1초 후에 로그인 결과를 설정합니다. (실제로는 서버와 통신해야 함)
+  };
+
+  return (
+    <main className={style.LoginBackImg}>
+      <form>
+        <div className={style.Inputdiv}>
+          <div className={style.Loginsub} style={{ backgroundImage: `url(${LoginID})` }}>
+            <input type="text" id="ID" name="ID" placeholder="ID" required />
+          </div>
+
+          <div className={style.Loginsub} style={{ backgroundImage: `url(${LoginPW})` }}>
+            <input type="password" id="pw" name="pw" placeholder="pw" required />
+          </div>
+        </div>
+        <div className={style.LoginBtdiv}>
+          <img src={LoginBt} alt="LoginBt" className={style.LoginBt} onClick={handleLogin} />
+        </div>
+        {loginStatus === false && <p>Login Failed. Please try again.</p>}
+      </form>
+    </main>
+  );
 };
 
 export default FoodLogin;
