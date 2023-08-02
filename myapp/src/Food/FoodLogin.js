d3cc7a3c0421323b070faa5e3cad5e61a727769f
YeDongVibe <wkwhstladmss@naver.com>
2023-08-02 17:20:57 +0900

    23.08.02

--- myapp/src/Food/FoodLogin.js
+++ myapp/src/Food/FoodLogin.js
@@ -1,29 +1,52 @@
 import style from "./Food.module.css";
 import Login from "../Img/FoodLogin/Login.png";
 import LoginsubBack from "../Img/FoodLogin/LoginsubBack.png"
-import LoginBt from "../Img/FoodLogin/LoginBt.png";
-import LoginCreate from "../Img/FoodLogin/LoginCreate.png"
+import { Link } from "react-router-dom";
 
 
 
 
 const FoodLogin = () => {
+  const handleCreateClick = () => {
+    alert("회원가입은 웹페이지에서만 가능합니다.");
+  };
 
   return (
     <main className={style.LoginBackImg}>
-      <div><img src={LoginsubBack} className={style.LoginsubBack} /></div>
+      <div>
+        <img src={LoginsubBack} className={style.LoginsubBack} />
+      </div>
       <form>
         <div className={style.Inputdiv}>
-          <div className={style.LoginsubId} >
-            <input type="email" id="Email" placeholder="Email" img src={Login} required />
+          <div className={style.LoginsubId}>
+            <input
+              type="email"
+              id="Email"
+              placeholder="Email"
+              img
+              src={Login}
+              required
+            />
           </div>
-          <div className={style.LoginsubPw} style={{ backgroundImage: `url(${Login})` }}>
-            <input type="password" id="pw" name="pw" placeholder="Password" required />
+          <div
+            className={style.LoginsubPw}
+            style={{ backgroundImage: `url(${Login})` }}
+          >
+            <input
+              type="password"
+              id="pw"
+              name="pw"
+              placeholder="Password"
+              required
+            />
           </div>
         </div>
 
         <button className={style.LoginBt} />
-        <button className={style.LoginCreate} />
+        {/* 'LoginCreate' 버튼을 누를 때 경고창을 띄우고, FoodJoin.js로 연결 */}
+        <Link to="/foodjoin">
+          <button className={style.LoginCreate} onClick={handleCreateClick} />
+        </Link>
       </form>
     </main>
   );
